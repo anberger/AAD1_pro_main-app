c844e4d82593f024d9b22bf71a4184d595c580b5
anberger <bergerandy@hotmail.com>
2014-10-26 21:56:28 +0100

    refactoring & finalization for handin

--- AAD1_pro_main-app/src/com/aad1/aad1_pro_main_app/ActivityButtons.java
+++ AAD1_pro_main-app/src/com/aad1/aad1_pro_main_app/ActivityButtons.java
@@ -1,6 +1,7 @@
 package com.aad1.aad1_pro_main_app;
 
 import com.google.gson.JsonObject;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -40,6 +41,7 @@ public class ActivityButtons extends Activity {
 	private int valRight = 0;
 	private int valLeft = 0;
 
+	@SuppressLint("ClickableViewAccessibility") 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_buttons);
